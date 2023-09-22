81839e0307df1fb369e08428511f6e46c7111f49
ilmu23 <ivalimak@student.hive.fi>
2023-09-22 21:45:33 +0300

    Added ex12 and commented out tests in ex11

--- ex12/ft_iterative_factorial.c
+++ ex12/ft_iterative_factorial.c
@@ -1,38 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_div_mod.c                                       :+:      :+:    :+:   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ivalimak <ivalimak@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/22 21:22:49 by ivalimak          #+#    #+#             */
-/*   Updated: 2023/09/22 21:31:27 by ivalimak         ###   ########.fr       */
+/*   Created: 2023/09/22 21:33:29 by ivalimak          #+#    #+#             */
+/*   Updated: 2023/09/22 21:45:10 by ivalimak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_div_mod(int a, int b, int *div, int *mod)
+int	ft_iterative_factorial(int nb)
 {
-	*div = a / b;
-	*mod = a % b;
+	int	sum;
+	int	i;
+
+	i = 1;
+	sum = 1;
+	while (i <= nb)
+		sum = sum * i++;
+	return (sum);
 }
 /*
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-	int	a;
-	int	b;
-	int	d;
-	int	r;
-
-	if (argc != 3)
+	int	nbr;
+	if (argc != 2)
 		return (1);
-	a = atoi(argv[1]);
-	b = atoi(argv[2]);
-	ft_div_mod(a, b, &d, &r);
-	printf("%d / %d = %d (remainder %d)\n", a, b, d, r);
-	ft_div_mod(b, a, &d, &r);
-	printf("%d / %d = %d (remainder %d)\n", b, a, d, r);
+	nbr = atoi(argv[1]);
+	printf("%d! = %d\n", nbr, ft_iterative_factorial(nbr));
 	return (0);
 }*/
