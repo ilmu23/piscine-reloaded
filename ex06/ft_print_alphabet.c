94240746851575fd7a89d3fb33c99d6c2d269a24
ilmu23 <ivalimak@student.hive.fi>
2023-09-22 18:01:33 +0300

    Added ex00 - ex06 (-ex04)

---
+++ ex06/ft_print_alphabet.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ivalimak <ivalimak@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/22 17:34:44 by ivalimak          #+#    #+#             */
+/*   Updated: 2023/09/22 18:00:20 by ivalimak         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	ft_print_alphabet(void)
+{
+	char	c;
+
+	c = 'a';
+	while (c < 123)
+		ft_putchar(c++);
+}
+/*
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int	main(void)
+{
+	ft_print_alphabet();
+	return (0);
+}*/
