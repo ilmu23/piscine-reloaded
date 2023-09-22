30fea27b84bd385d5bf23b87b9ecc02d63bee94f
ilmu23 <ivalimak@student.hive.fi>
2023-09-22 21:16:31 +0300

    Added ex09

---
+++ ex09/ft_ft.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ft.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ivalimak <ivalimak@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/22 21:12:26 by ivalimak          #+#    #+#             */
+/*   Updated: 2023/09/22 21:16:05 by ivalimak         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_ft(int *nbr)
+{
+	*nbr = 42;
+}
+/*
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+	int	n;
+
+	if (argc != 2)
+		return (1);
+	n = atoi(argv[1]);
+	printf("n = %d\n", n);
+	ft_ft(&n);
+	printf("n = %d\n", n);
+	return (0);
+}*/
