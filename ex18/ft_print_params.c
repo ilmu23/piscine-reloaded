1ecdda749ef821dbf240b8512310f5ba820457a8
ilmu23 <ivalimak@student.hive.fi>
2023-09-22 22:40:46 +0300

    Added ex18

---
+++ ex18/ft_print_params.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ivalimak <ivalimak@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/22 22:36:54 by ivalimak          #+#    #+#             */
+/*   Updated: 2023/09/22 22:39:31 by ivalimak         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	ft_putstr(char *s)
+{
+	while (*s)
+		ft_putchar(*s++);
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	i = 1;
+	while (i < argc)
+	{
+		ft_putstr(argv[i++]);
+		ft_putchar('\n');
+	}
+	return (0);
+}
